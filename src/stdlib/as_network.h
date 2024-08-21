94db5f9a36e132abb4eb459c3ddc8a7e1309f6ed
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 18:05:40 +0300

    Update as_network.h

--- src/stdlib/as_network.h
+++ src/stdlib/as_network.h
@@ -3,11 +3,33 @@
 
 #include <string>
 
+/**
+ * @brief Класс для работы с сетевыми операциями.
+ * 
+ * Предоставляет методы для подключения к серверу, отправки и получения данных.
+ */
 class NetworkClient {
 public:
+    /**
+     * @brief Подключение к серверу.
+     * 
+     * @param address Адрес сервера для подключения.
+     */
     void connect(const std::string& address);
+
+    /**
+     * @brief Отправка данных на сервер.
+     * 
+     * @param data Данные, которые необходимо отправить.
+     */
     void sendData(const std::string& data);
+
+    /**
+     * @brief Получение данных от сервера.
+     * 
+     * @return std::string Данные, полученные от сервера.
+     */
     std::string receiveData();
 };
 
-#endif
+#endif // AS_NETWORK_H
