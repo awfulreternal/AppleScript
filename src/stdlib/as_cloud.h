fa46e1eff36b86b2c3d0741b182cc15e2ce608f9
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 17:59:18 +0300

    Update as_cloud.h

--- src/stdlib/as_cloud.h
+++ src/stdlib/as_cloud.h
@@ -3,10 +3,25 @@
 
 #include <string>
 
+/**
+ * @class CloudServiceClient
+ * @brief Класс для работы с облачными сервисами, позволяющий загружать и скачивать файлы.
+ */
 class CloudServiceClient {
 public:
+    /**
+     * @brief Загружает файл на указанный URL.
+     * @param filePath Путь к файлу, который нужно загрузить.
+     * @param url URL, куда файл будет загружен.
+     */
     void uploadFile(const std::string& filePath, const std::string& url);
+
+    /**
+     * @brief Скачивает файл с указанного URL.
+     * @param url URL, откуда файл будет скачан.
+     * @param outputPath Путь, куда будет сохранен скачанный файл.
+     */
     void downloadFile(const std::string& url, const std::string& outputPath);
 };
 
-#endif
+#endif // AS_CLOUD_H
