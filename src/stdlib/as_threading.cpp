e3e2423b22199d077325cdb0744697af3fecc0a1
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 18:09:25 +0300

    Update as_threading.cpp

--- src/stdlib/as_threading.cpp
+++ src/stdlib/as_threading.cpp
@@ -1,17 +1,41 @@
 #include "as_threading.h"
+#include <iostream>
 
 namespace as {
 
-Thread::Thread(std::function<void()> func) : thread(func) {}
+// Конструктор класса Thread, принимает функцию для выполнения в новом потоке
+Thread::Thread(std::function<void()> func) : thread(func), isRunning(false) {}
 
+// Метод для запуска потока
 void Thread::start() {
-    thread = std::thread([this] { thread.join(); });
+    if (!isRunning) {
+        // Запускаем поток и устанавливаем флаг, что поток запущен
+        thread = std::thread([this] {
+            if (func) {
+                func();  // Выполнение переданной функции
+            }
+        });
+        isRunning = true;
+    } else {
+        std::cerr << "Thread is already running!" << std::endl;
+    }
 }
 
+// Метод для ожидания завершения потока
 void Thread::join() {
-    if (thread.joinable()) {
-        thread.join();
+    if (isRunning) {
+        if (thread.joinable()) {
+            thread.join();  // Ожидание завершения потока
+        }
+        isRunning = false;
+    } else {
+        std::cerr << "Thread is not running or already joined!" << std::endl;
     }
+}
+
+// Метод для проверки, запущен ли поток
+bool Thread::isRunning() const {
+    return isRunning;
 }
 
 } // namespace as
