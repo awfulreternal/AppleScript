a810ba11dc6333fae5f27ead66ecd9469985c99b
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 18:09:41 +0300

    Update as_threading.h

--- src/stdlib/as_threading.h
+++ src/stdlib/as_threading.h
@@ -6,16 +6,27 @@
 
 namespace as {
 
+// Класс для многопоточности
 class Thread {
 public:
-    Thread(std::function<void()> func);
+    // Конструктор, принимающий функцию для выполнения в потоке
+    explicit Thread(std::function<void()> func);
+
+    // Метод для запуска потока
     void start();
+
+    // Метод для ожидания завершения потока
     void join();
 
+    // Метод для проверки, запущен ли поток
+    bool isRunning() const;
+
 private:
-    std::thread thread;
+    std::thread thread;   // Объект потока
+    std::function<void()> func;  // Функция, выполняемая в потоке
+    bool isRunning;  // Флаг, указывающий, запущен ли поток
 };
 
 } // namespace as
 
-#endif
+#endif // AS_THREADING_H
