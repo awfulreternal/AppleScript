855abad584de0a2ec0999673544913265ad96261
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 18:06:15 +0300

    Update as_stdio.cpp

--- src/stdlib/as_stdio.cpp
+++ src/stdlib/as_stdio.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 
 namespace as {
 
+/**
+ * @brief Выводит сообщение в стандартный вывод.
+ * 
+ * Использует std::cout для вывода строки с последующим переходом на новую строку.
+ * 
+ * @param message Строка, которая будет выведена на экран.
+ */
 void print(const std::string& message) {
     std::cout << message << std::endl;
 }
