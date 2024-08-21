412a7c05c3a955c3f28fa6bd80dff884692bd27b
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 17:55:26 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <memory>
 #include "lexer.h"
 #include "parser.h"
 #include "codegen.h"
 #include "optimizer.h"
 #include "linker.h"
+#include "error_handler.h"
 
 int main(int argc, char* argv[]) {
+    // Проверка аргументов командной строки
     if (argc < 2) {
         std::cerr << "Usage: applescript <sourcefile>" << std::endl;
         return 1;
@@ -13,10 +16,26 @@ int main(int argc, char* argv[]) {
 
     // Лексический анализ
     Lexer lexer(argv[1]);
-    Parser parser(lexer);
+    if (g_errorHandler.hasErrors()) {
+        g_errorHandler.printErrors();
+        return 1;
+    }
 
     // Парсинг исходного кода в AST
-    ASTNode* root = parser.parse();
+    Parser parser(lexer);
+    ASTNode* root = nullptr;
+    try {
+        root = parser.parse();
+    } catch (const std::exception& e) {
+        g_errorHandler.addError(ErrorType::SYNTAX_ERROR, e.what(), 0, 0); // Параметры строки и столбца можно уточнить
+        g_errorHandler.printErrors();
+        return 1;
+    }
+
+    if (g_errorHandler.hasErrors()) {
+        g_errorHandler.printErrors();
+        return 1;
+    }
 
     // Генерация промежуточного кода
     CodeGenerator codegen;
@@ -28,9 +47,24 @@ int main(int argc, char* argv[]) {
 
     // Линковка и компиляция в объектный код
     Linker linker;
-    linker.link(codegen.getObjectCode(), "output.o");
+    try {
+        linker.link(codegen.getObjectCode(), "output.o");
+    } catch (const std::exception& e) {
+        g_errorHandler.addError(ErrorType::RUNTIME_ERROR, e.what(), 0, 0); // Параметры строки и столбца можно уточнить
+        g_errorHandler.printErrors();
+        return 1;
+    }
+
+    if (g_errorHandler.hasErrors()) {
+        g_errorHandler.printErrors();
+        return 1;
+    }
 
     std::cout << "Compilation finished: output.o" << std::endl;
+
+    // Очистка ресурсов
+    delete root; // Удаление дерева AST
+    g_errorHandler.clear(); // Очистка обработчика ошибок
 
     return 0;
 }
