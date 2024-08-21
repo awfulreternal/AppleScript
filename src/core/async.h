2550429fea4ce96557eb805e3f80e7d7b109242a
awfulreternal <matveykudelin7@gmail.com>
2024-08-21 17:54:22 +0300

    Create async.h

---
+++ src/core/async.h
@@ -0,0 +1,12 @@
+#ifndef ASYNC_H
+#define ASYNC_H
+
+#include <functional>
+
+class Async {
+public:
+    // Метод для обработки асинхронных операций
+    static void handleAsync();
+};
+
+#endif // ASYNC_H
