49ed083475b2834762bdde5182b53a8bb7c0c8e2
awfulreternal <matveykudelin7@gmail.com>
2024-08-22 18:19:28 +0300

    Update logger.h

--- include/logger.h
+++ include/logger.h
@@ -4,26 +4,42 @@
 #include <string>
 #include <mutex>  // Для потокобезопасности
 
+/**
+ * @class Logger
+ * @brief Класс для вывода сообщений в лог.
+ * 
+ * Поддерживает информационные, отладочные и сообщения об ошибках. Отладочные сообщения выводятся
+ * только если установлен режим verbose.
+ */
 class Logger {
 public:
-    // Вывод информационного сообщения
+    /**
+     * @brief Вывод информационного сообщения.
+     * @param message Сообщение для вывода.
+     */
     static void info(const std::string& message);
 
-    // Вывод сообщения об ошибке
+    /**
+     * @brief Вывод сообщения об ошибке.
+     * @param message Сообщение для вывода.
+     */
     static void error(const std::string& message);
 
-    // Вывод отладочного сообщения, если включен режим verbose
+    /**
+     * @brief Вывод отладочного сообщения, если включен режим verbose.
+     * @param message Сообщение для вывода.
+     */
     static void debug(const std::string& message);
 
-    // Установка режима verbose
+    /**
+     * @brief Установка режима verbose.
+     * @param verbose Флаг, указывающий, следует ли включить режим verbose.
+     */
     static void setVerbose(bool verbose);
 
 private:
-    // Статическая переменная для хранения режима verbose
-    static bool verbose;
-
-    // Мьютекс для обеспечения потокобезопасности
-    static std::mutex mutex;
+    static bool verbose;        ///< Статическая переменная для хранения режима verbose.
+    static std::mutex mutex;    ///< Мьютекс для обеспечения потокобезопасности.
 };
 
 #endif // LOGGER_H
