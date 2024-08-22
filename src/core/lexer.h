ce0a834bf8226b9ec9cb73e7ebaa8a08cd31cbf2
awfulreternal <matveykudelin7@gmail.com>
2024-08-22 14:04:30 +0300

    Update lexer.h

--- src/core/lexer.h
+++ src/core/lexer.h
@@ -2,7 +2,7 @@
 #define LEXER_H
 
 #include <string>
-#include <unordered_map>
+#include <unordered_set>
 
 // Определение типов токенов
 enum TokenType {
@@ -14,31 +14,50 @@ enum TokenType {
     TOKEN_CLASS,
     TOKEN_FUNCTION,
     TOKEN_ASYNC,
-    TOKEN_AWAIT
+    TOKEN_AWAIT,
+    TOKEN_IF,
+    TOKEN_ELSE,
+    TOKEN_FOR,
+    TOKEN_WHILE
 };
 
 // Структура токена
 struct Token {
-    TokenType type;
-    std::string value;
+    TokenType type;  // Тип токена
+    std::string value;  // Значение токена
+
+    Token(TokenType type = TOKEN_EOF, const std::string& value = "")
+        : type(type), value(value) {}
 };
 
-// Класс лексера
+// Класс лексера для обработки исходного кода
 class Lexer {
 public:
-    Lexer(const std::string& filename);
+    // Конструктор лексера, принимающий имя файла
+    explicit Lexer(const std::string& filename);
+
+    // Получение следующего токена из исходного кода
     Token getNextToken();
 
 private:
+    // Перемещение к следующему символу в исходном коде
     void advance();
+
+    // Обработка идентификаторов и ключевых слов
     Token identifier();
+
+    // Обработка чисел
     Token number();
+
+    // Обработка операторов
     Token op();
+
+    // Обработка знаков препинания
     Token punct();
 
-    std::string source;
-    size_t currentIndex;
-    char currentChar;
+    std::string source;  // Исходный код
+    size_t currentIndex;  // Индекс текущего символа
+    char currentChar;  // Текущий символ
 };
 
 #endif // LEXER_H
