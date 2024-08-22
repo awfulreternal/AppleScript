81665d09d744e8f2c31fce6d3712569d5eb1da0e
awfulreternal <matveykudelin7@gmail.com>
2024-08-22 18:30:01 +0300

    Update arm.h

--- include/arch/arm.h
+++ include/arch/arm.h
@@ -16,6 +16,8 @@ private:
     // Утилитные функции
     void generateBinaryOperationCode(const std::string& operation, ASTNode* left, ASTNode* right);
     void generateMoveInstruction(const std::string& value);
+    void generatePushRegister(const std::string& reg);
+    void generatePopRegister(const std::string& reg);
 };
 
 #endif // ARM_H
