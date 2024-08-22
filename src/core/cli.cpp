1c4a19e9c1d5bd79d6a39a89a6a759a554ee69be
awfulreternal <matveykudelin7@gmail.com>
2024-08-22 13:56:49 +0300

    Update cli.cpp

--- src/core/cli.cpp
+++ src/core/cli.cpp
@@ -1,12 +1,16 @@
 #include "cli.h"
 #include <iostream>
+#include <algorithm> // Для std::find_if
 
 CLI::CLI(int argc, char* argv[])
     : argc(argc), argv(argv), verbose(false), optimize(false) {}
 
 bool CLI::parseArguments() {
+    bool hasSourceFile = false;
+
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
+        
         if (arg == "--verbose") {
             verbose = true;
         } else if (arg == "--optimize") {
@@ -18,11 +22,14 @@ bool CLI::parseArguments() {
             std::cout << "  --optimize     Enable code optimization\n";
             std::cout << "  --help         Show this help message\n";
             return false;
-        } else {
+        } else if (arg.find_last_of('.') != std::string::npos) {
+            // Assuming that any argument with a dot is a source file
             sourceFile = arg;
+            hasSourceFile = true;
         }
     }
-    return !sourceFile.empty();
+
+    return hasSourceFile;
 }
 
 std::string CLI::getSourceFile() const {
